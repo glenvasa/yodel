ad92f1240ca85c9906aacbcaa891b5900756b647
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-27 15:31:54 -0500

    added icons below tweet input; added emoji-mart which displays when EmojiHappyIcon clicked

--- pages/components/Input.js
+++ pages/components/Input.js
@@ -1,9 +1,30 @@
-import { XIcon } from "@heroicons/react/outline";
-import { useState } from "react";
+import { CalendarIcon, ChartBarIcon, EmojiHappyIcon, PhotographIcon, XIcon } from "@heroicons/react/outline";
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css"
+import { useState, useRef } from "react";
 
 function Input() {
   const [input, setInput] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
+  const [showEmojis, setShowEmojis] = useState(false)
+
+  const filePickerRef = useRef(null)
+
+  const addImageToPost = () => {
+
+  }
+
+  const sendPost = () => {
+      
+  }
+
+  const addEmoji = (e) => {
+    let sym = e.unified.split("-");
+    let codesArray = [];
+    sym.forEach((el) => codesArray.push("0x" + el));
+    let emoji = String.fromCodePoint(...codesArray);
+    setInput(input + emoji);
+  };
 
   return (
     <div
@@ -25,7 +46,8 @@ function Input() {
             <div className="relative">
               <div
                 className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex 
-              items-center justify-center top-1 left-1 cursor-pointer" onClick={() => setSelectedFile(null)}
+              items-center justify-center top-1 left-1 cursor-pointer"
+                onClick={() => setSelectedFile(null)}
               >
                 <XIcon className="text-white h-5" />
               </div>
@@ -36,6 +58,51 @@ function Input() {
               />
             </div>
           )}
+        </div>
+        <div className="flex items-center justify-between pt-2.5">
+          <div className="flex items-center">
+            <div className="icon" onClick={() => filePickerRef.current.click()}>
+              <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
+              <input
+                type="file"
+                ref={filePickerRef}
+                hidden
+                onChange={addImageToPost}
+              />
+            </div>
+            <div className="icon rotate-90">
+                <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
+              </div>
+
+              <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+                <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
+              </div>
+
+              <div className="icon">
+                <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
+              </div>
+
+              {showEmojis && (
+                <Picker
+                  onSelect={addEmoji}
+                  style={{
+                    position: "absolute",
+                    marginTop: "465px",
+                    marginLeft: -40,
+                    maxWidth: "320px",
+                    borderRadius: "20px",
+                  }}
+                  theme="dark"
+                />
+              )}
+          </div>
+          <button
+              className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
+              disabled={!input && !selectedFile}
+              onClick={sendPost}
+            >
+              Tweet
+            </button>
         </div>
       </div>
     </div>
