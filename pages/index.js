16f5607b3b9cde67ec7da603dbf88eb4a36ddba3
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-29 12:34:43 -0500

    imported Modal component

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import { getProviders, getSession, useSession } from "next-auth/react";
 import Login from '../components/Login';
+import Modal from '../components/Modal';
 
 export default function Home({trendingResults, followResults, providers}) {
   const {data: session} = useSession()
@@ -20,7 +21,7 @@ export default function Home({trendingResults, followResults, providers}) {
     <Feed />
     {/* Widgets */}
 
-    {/* Modal */}
+    <Modal />
      </main>
     </div>
   )
