43052d5fad2184cc1535aea90d3c7abab7918e43
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-30 10:01:24 -0500

    Displaying Widgets Component which displays Trending results (in Trending Component) and followResults from data fetched in getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
-import Head from 'next/head'
-import Feed from '../components/Feed'
-import Sidebar from '../components/Sidebar'
+import Head from "next/head";
+import Feed from "../components/Feed";
+import Sidebar from "../components/Sidebar";
 import { getProviders, getSession, useSession } from "next-auth/react";
-import Login from '../components/Login';
-import Modal from '../components/Modal';
+import Login from "../components/Login";
+import Modal from "../components/Modal";
 import { modalState } from "../atoms/modalAtom";
 import { useRecoilState } from "recoil";
+import Widgets from "../components/Widgets";
 
-export default function Home({trendingResults, followResults, providers}) {
-  const {data: session} = useSession()
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session } = useSession();
   const [isOpen, setIsOpen] = useRecoilState(modalState);
 
-  if (!session) return <Login providers={providers}/>
+  if (!session) return <Login providers={providers} />;
 
   return (
     <div className="">
@@ -19,24 +20,27 @@ export default function Home({trendingResults, followResults, providers}) {
         <title>Yodel</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-     <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
-    <Sidebar />
-    <Feed />
-    {/* Widgets */}
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+        <Sidebar />
+        <Feed />
+        <Widgets
+          trendingResults={trendingResults}
+          followResults={followResults}
+        />
 
-    {isOpen && <Modal />}
-     </main>
+        {isOpen && <Modal />}
+      </main>
     </div>
-  )
+  );
 }
 
 export async function getServerSideProps(context) {
-  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
-    (res) => res.json()
-  );
-  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
-    (res) => res.json()
-  );
+  const trendingResults = await fetch(
+    "https://jsonkeeper.com/b/NKEV"
+  ).then((res) => res.json());
+  const followResults = await fetch(
+    "https://jsonkeeper.com/b/WWMJ"
+  ).then((res) => res.json());
   const providers = await getProviders();
   const session = await getSession(context);
 
